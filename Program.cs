2e197393f0a33f0a576f22f6deb6decd035568dc
wildanabe31 <fadilalfadil091@gmail.com>
2026-02-09 10:09:38 +0700

    penambahan energi

--- Program.cs
+++ Program.cs
@@ -5,10 +5,45 @@ Console.WriteLine("Permainan Dimulai... \n");
 wizardA.ShowStacts();
 wizardB.ShowStacts();
 
-wizardA.Attack(wizardB);
-wizardB.Attack(wizardA);
-wizardA.Attack(wizardB);
-wizardB.Attack(wizardA);
+//wizardA.Attack(wizardB);
+//wizardB.Attack(wizardA);
+//wizardA.Attack(wizardB);
+//wizardB.Attack(wizardA);
+
+string pilihan;
+
+while (wizardA.Energy > 0 && wizardB.Energy > 0)
+{
+    Console.WriteLine($"1. {wizardA.Name} menyerang {wizardB}");
+    Console.WriteLine($"2. {wizardB.Name} menyerang {wizardA}");
+    Console.WriteLine($"3. {wizardA} melakukan heal");
+    Console.WriteLine($"4. {wizardA} melakukan heal");
+
+    Console.WriteLine("\nMasukan pilihanmu (1/2/3/4): ");
+
+    pilihan = Console.ReadLine();
+
+    if (pilihan == "1")
+    {
+        wizardA.Attack(wizardB);
+    }
+    else if (pilihan == "2")
+    {
+        wizardB.Attack(wizardA);
+    }
+    else if(pilihan == "3")
+    {
+        wizardA.Heal();
+    }
+    else if (pilihan == "4")
+    {
+        wizardB.Heal();
+    }
+    else
+    {
+        Console.WriteLine("Pilihan tidak valid");
+    }
+}
 
 Console.WriteLine("Permainan Berakhir...\n");
 wizardA.ShowStacts();
@@ -42,9 +77,11 @@ public class Wizard
         Console.WriteLine($"Sisa energi {wizardLawanObj.Name} adalah {wizardLawanObj.Name}");
     }
 
-    public void Heal(Wizard wizardLawanOBJ)
+    public void Heal()
     {
-        if (Energy < 100)
+        Energy += 5;
+
+        if (Energy <= 100)
         {
             Console.WriteLine($"{Name} melakukan heal! Energi meningkat menjadi {Energy}");
         }
